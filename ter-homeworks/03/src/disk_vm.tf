ae6eb75b3946f0443ae39929f35debde91a4c8da
al.bugrov <al.bugrov@agatgroup.com>
2024-02-03 10:53:02 +0300

    Fix homework 03

--- ter-homeworks/03/src/disk_vm.tf
+++ ter-homeworks/03/src/disk_vm.tf
@@ -31,7 +31,7 @@ resource "yandex_compute_instance" "storage" {
   dynamic "secondary_disk" {
     for_each = yandex_compute_disk.disks_storage
     content {
-      disk_id     = yandex_compute_disk.disks_storage[secondary_disk.key].id
+      disk_id     = secondary_disk.value.id
       device_name = secondary_disk.value.name
       auto_delete = var.stor_auto_delete
     }
