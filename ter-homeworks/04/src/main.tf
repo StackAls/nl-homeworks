c8f902e37c817ac4e3309f1a308200332237f1b9
al.bugrov <al.bugrov@agatgroup.com>
2024-02-03 13:58:07 +0300

    Homework 04 task 1

---
+++ ter-homeworks/04/src/main.tf
@@ -0,0 +1,61 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+module "marketing" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = var.marketing
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "web"
+  instance_count = 1
+  image_family   = var.image_family
+  public_ip      = true
+
+  labels = {
+    env = var.marketing
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+    serial-port-enable = 1
+  }
+
+}
+
+module "analytics" {
+  source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name       = var.analytics
+  network_id     = yandex_vpc_network.develop.id
+  subnet_zones   = [var.default_zone]
+  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  instance_name  = "web"
+  instance_count = 1
+  image_family   = var.image_family
+  public_ip      = true
+
+  labels = {
+    env = var.analytics
+  }
+
+  metadata = {
+    user-data          = data.template_file.cloudinit.rendered
+    serial-port-enable = 1
+  }
+
+}
+
+data "template_file" "cloudinit" {
+  template = file("${path.module}/cloud-init.yml.tftpl")
+  vars = {
+    ssh_public_key = local.ssh-keys
+  }
+}
